cfe8a086e617da52f44c459a803fe21395cc512e
byron <byronap120@gmail.com>
2018-08-26 00:54:34 -0600

    Enemies
    - Enemies
    - gun and bullet

---
+++ Assets/Scripts/EnemyFire.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFire : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+    public float enemySpeed = 10f;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(FireObject());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+   
+    }
+
+    IEnumerator FireObject()
+    {
+        while(true)
+        {
+            int timeToWait = Random.Range(1, 4);
+            yield return new WaitForSeconds(timeToWait);
+
+            // Create the Bullet from the Bullet Prefab
+            var bullet = (GameObject)Instantiate(
+                enemyPrefab,
+                this.transform.position,
+                this.transform.rotation);
+
+            // Add velocity to the bullet
+            bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * enemySpeed;
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float bulletSpeed = 8f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            Debug.Log("com.byron:.PrimaryIndexTrigger");
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+}
