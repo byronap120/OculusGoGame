28b84c450b5478e46345d4fec25757dc68831da6
byron <byronap120@gmail.com>
2018-09-01 18:50:16 -0600

    Improvement on Scripts for enemies

---
+++ Assets/Scripts/EnemyGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+
+    public GameObject enemyPrefab;
+    public bool useGravity;
+    public float enemySpeed = 10f;
+    public int timeToWaitForNextEnemy = 2;
+
+    private AudioSource enemyAudioSource;
+
+    void Start()
+    {
+        //Fetch the AudioSource from the GameObject
+        enemyAudioSource = GetComponent<AudioSource>();
+        StartCoroutine(FireObject());
+    }
+
+    IEnumerator FireObject()
+    {
+        while (true)
+        {
+            int timeToWait = Random.Range(1, timeToWaitForNextEnemy);
+            yield return new WaitForSeconds(timeToWait);
+
+            // Create the enemy from the enemy Prefab
+            var bullet = (GameObject)Instantiate(
+                enemyPrefab,
+                this.transform.position,
+                this.transform.rotation);
+
+            // Add velocity to the enemy
+            Rigidbody bulletRigidBody = bullet.GetComponent<Rigidbody>();
+            bulletRigidBody.useGravity = useGravity;
+            bulletRigidBody.velocity = bullet.transform.forward * enemySpeed;
+
+            // Play sound
+            if (enemyAudioSource != null)
+            {
+                enemyAudioSource.Play();
+            }
+        }
+
+    }
+}
